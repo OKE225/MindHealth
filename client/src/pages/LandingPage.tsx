3d46ff1a8eb15ef2b1f8fbb6e52f5f98585b955f
OKE225 <patryk.wroblewski.2006@gmail.com>
2025-11-10 19:28:10 +0100

    Add wave animated background

--- client/src/pages/LandingPage.tsx
+++ client/src/pages/LandingPage.tsx
@@ -1,17 +1,26 @@
+import { Link } from "react-router";
+import AnimatedBackground from "../components/AnimatedBackground";
+import Header from "../components/Header";
+
 const LandingPage = () => {
   return (
-    <div>
-      <h1 className="font-bold text-5xl">MindHealth</h1>
-      <p>Twoje wsparcie dla zdrowia psychicznego każdego dnia</p>
-      <button className="bg-stone-400 p-2 rounded-xl">
-        Przejdź do aplikacji
-      </button>
-      <p>wprowadzenie że zdrowie psychiczne jest ważne i warto o nie dbać</p>
-      <p>
-        krótkie wprowadzenie co to jest mindfulness, terapia
-        poznawczo-behawioralna (CBT)
-      </p>
-    </div>
+    <>
+      <AnimatedBackground />
+      <div>
+        <Header />
+        <p>Twoje wsparcie dla zdrowia psychicznego każdego dnia</p>
+        <Link
+          to="/application"
+          className="inline-block bg-sky-500 text-white p-2 rounded-lg cursor-pointer">
+          Przejdź do aplikacji
+        </Link>
+        <p>wprowadzenie że zdrowie psychiczne jest ważne i warto o nie dbać</p>
+        <p>
+          krótkie wprowadzenie co to jest mindfulness, terapia
+          poznawczo-behawioralna (CBT)
+        </p>
+      </div>
+    </>
   );
 };
 
