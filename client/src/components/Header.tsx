3ec52fca532e5f08d4fd36eb4783c7d58aba3106
OKE225 <patryk.wroblewski.2006@gmail.com>
2025-11-11 17:11:47 +0100

    Change design

--- client/src/components/Header.tsx
+++ client/src/components/Header.tsx
@@ -2,9 +2,9 @@ import brainIcon from "../assets/icons8-brain.png";
 
 const Header = () => {
   return (
-    <div className="flex items-center">
+    <div className="flex items-center cursor-default">
       <img className="w-10 h-10" src={brainIcon} alt="" />
-      <h1 className="font-semibold text-5xl ml-1">MindHealth</h1>
+      <h1 className="font-light text-4xl ml-1">MindHealth</h1>
     </div>
   );
 };
--- client/src/components/WelcomeMessage.tsx
+++ client/src/components/WelcomeMessage.tsx
@@ -1,9 +1,11 @@
 const WelcomeMessage = () => {
   return (
-    <>
-      <h3>Cześć! Miło Cię widzieć.</h3>
-      <p>Rozpocznij swoją podróż w kierunku lepszego zdrowia psychicznego.</p>
-    </>
+    <div className="text-center mt-10">
+      <h3 className="text-4xl font-medium">Cześć! Miło Cię widzieć.</h3>
+      <p className="text-stone-600 mt-2">
+        Rozpocznij swoją podróż w kierunku lepszego zdrowia psychicznego.
+      </p>
+    </div>
   );
 };
 
