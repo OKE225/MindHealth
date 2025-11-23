b1094eca34ba3130612764810c24187a0ff245d7
OKE225 <patryk.wroblewski.2006@gmail.com>
2025-11-23 22:15:35 +0100

    Add two sections into home page

---
+++ client/src/components/MindfulnessExercises/RandomExercises.tsx
@@ -0,0 +1,48 @@
+import { useContext, useEffect, useState } from "react";
+import { ApplicationContext } from "../../ApplicationContext";
+import ExerciseCard from "./ExerciseCard";
+import type { MindfulnessExercise } from "../../types/MindfulnessExercise";
+
+const RandomExercises = () => {
+  const context = useContext(ApplicationContext);
+  if (!context)
+    throw new Error(
+      "MindfulnessExercisesList must be used within DiaryContext.Provider"
+    );
+
+  const { exercisesList, isLoadingExercises } = context;
+
+  const [randomExercises, setRandomExercises] = useState<
+    MindfulnessExercise[] | []
+  >([]);
+
+  useEffect(() => {
+    const getRandomExercise = () => {
+      const result = [];
+      const newArray = [...exercisesList];
+
+      for (let i = 0; i < 3; i++) {
+        const randomIndex = Math.floor(Math.random() * newArray.length);
+        result.push(newArray[randomIndex]);
+        newArray.splice(randomIndex, 1);
+      }
+
+      return result;
+    };
+
+    setRandomExercises(getRandomExercise());
+  }, [exercisesList]);
+
+  return isLoadingExercises ? (
+    <div className="w-12 h-12 mx-auto border-4 border-gray-200 border-t-blue-500 rounded-full animate-spin"></div>
+  ) : (
+    <div className="grid grid-cols-3 gap-2 ">
+      {/* [&>*:nth-child(3)]:hidden */}
+      {randomExercises.map((exercise, index) => (
+        <ExerciseCard key={index} {...exercise} />
+      ))}
+    </div>
+  );
+};
+
+export default RandomExercises;
--- client/src/pages/ApplicationPage.tsx
+++ client/src/pages/ApplicationPage.tsx
@@ -2,13 +2,15 @@ import AdvicesSection from "../components/AdvicesSection";
 import NavbarWithLinks from "../components/NavbarWithLinks";
 import SectionHeaders from "../components/SectionHeaders";
 import SelectYourMood from "../components/Mood/SelectYourMood";
+import SaveNote from "../components/Diary/SaveNote";
+import RandomExercises from "../components/MindfulnessExercises/RandomExercises";
 
 const ApplicationPage = () => {
   return (
     <div>
       <NavbarWithLinks />
 
-      <main className="w-[70%] mx-auto">
+      <main className="w-[70%] mx-auto pb-25">
         <SectionHeaders
           header="Cześć! Miło Cię Widzieć"
           description="Rozpocznij swoją podróż w kierunku lepszego zdrowia psychicznego."
@@ -16,6 +18,26 @@ const ApplicationPage = () => {
 
         <AdvicesSection />
         <SelectYourMood />
+
+        <div className="bg-white mt-5 p-5 rounded-2xl shadow">
+          <h2 className="text-stone-900 text-2xl font-bold">Jasność umysłu</h2>
+          <p className="text-stone-600 mb-5">
+            Codzienna praktyka pomaga redukować stres, poprawia koncentrację i
+            wspiera spokój ducha.
+          </p>
+          <RandomExercises />
+        </div>
+
+        <div className="bg-white mt-5 p-5 rounded-2xl shadow">
+          <h2 className="text-stone-900 text-2xl font-bold">
+            Zapisz notatkę do pamiętnika
+          </h2>
+          <p className="text-stone-600 mb-5">
+            Podziel się swoimi przemyśleniami, opowiedz, jak się dziś czujesz i
+            czego się boisz.
+          </p>
+          <SaveNote />
+        </div>
       </main>
     </div>
   );
